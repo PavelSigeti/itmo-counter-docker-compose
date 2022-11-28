524142f14818c3cee791c629f7fdcea14bb166e0
PavelSigeti <103564012+PavelSigeti@users.noreply.github.com>
2022-11-28 12:10:03 +0300

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `counter`;
 USE `counter`;
 CREATE TABLE `data` (`id` INT NOT NULL AUTO_INCREMENT, `datetime` DATETIME NOT NULL , `client_info` TEXT NOT NULL , PRIMARY KEY (`id`)); 
-
-INSERT INTO `data` (`datetime`, `client_info`) VALUES (now(), 'User-Agent: Mozilla/5.0 ');
