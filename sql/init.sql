e5d03378bb2ce0e87c3d2d9e59d168d6165e89d2
PavelSigeti <sigeti352@gmail.com>
2022-12-03 01:44:01 +0300

    Grant

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `counter`;
 USE `counter`;
 CREATE TABLE `data` (`id` INT NOT NULL AUTO_INCREMENT, `datetime` DATETIME NOT NULL , `client_info` TEXT NOT NULL , PRIMARY KEY (`id`)); 
+
+GRANT ALL PRIVILEGES ON counter.* to 'root'@'172.25.0.3';
