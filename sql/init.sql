31cd7d3095a9fca8d0bbe8a7b0b85e0ad6121ac4
PavelSigeti <sigeti352@gmail.com>
2022-12-03 01:53:24 +0300

    compose

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `counter`;
 USE `counter`;
 CREATE TABLE `data` (`id` INT NOT NULL AUTO_INCREMENT, `datetime` DATETIME NOT NULL , `client_info` TEXT NOT NULL , PRIMARY KEY (`id`)); 
-
-GRANT ALL PRIVILEGES ON counter.* to 'root'@'%';
