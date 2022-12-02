8454bbf209dea962e1efa5e7e537b5ab3702d616
PavelSigeti <sigeti352@gmail.com>
2022-12-03 01:56:52 +0300

    PHP

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php 
 
 try {
-    $dbh = new PDO('mysql:host=mysql;port=3306;dbname=counter', 'root', 'root');
+    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=counter', 'root', 'root');
     $dbh->query('SELECT count(*) FROM counter.data');
     $count = $dbh->query('SELECT count(*) FROM counter.data')->fetchColumn();
 
--- src/stat.php
+++ src/stat.php
@@ -5,7 +5,7 @@ echo $_SERVER['HTTP_USER_AGENT'];
 echo '<br>';
 
 try {
-    $dbh = new PDO('mysql:host=mysql;port=3306;dbname=counter', 'root', 'root');
+    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=counter', 'root', 'root');
     $count = $dbh->prepare("INSERT INTO counter.data (`datetime`, `client_info`) VALUES (now(), ?) ");
 
     $response = $count->execute([ $_SERVER['HTTP_USER_AGENT'] ]);
